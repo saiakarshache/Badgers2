f3ab652d64c99e1970b2ca5be5ef310ac30cc489
saiakarshache <168671202+saiakarshache@users.noreply.github.com>
2024-05-02 21:24:19 -0500

    update 3
    missing avg value function

--- BadgerHire2/data_analysis_app/experiment_data_processor/urls.py
+++ BadgerHire2/data_analysis_app/experiment_data_processor/urls.py
@@ -1,7 +1,25 @@
+# from django.urls import path
+# from .views import upload_csv, display_data
+
+# urlpatterns = [
+#     path('upload/', upload_csv, name='upload_csv'),
+#     path('data/', display_data, name='display_data'),
+# ]
+
+# from django.urls import path
+# from . import views  # Import the views module from the current package
+
+# urlpatterns = [
+#     path('upload/', views.upload_csv, name='upload_csv'),  # Use the views.upload_csv function
+#     path('data/', views.display_data, name='display_data'),
+# ]
+
 from django.urls import path
-from .views import upload_csv, display_data
+from . import views  # Import the views module from the current package
 
 urlpatterns = [
-    path('upload/', upload_csv, name='upload_csv'),
-    path('data/', display_data, name='display_data'),
+    path('upload/', views.upload_csv, name='upload_csv'),  # Use the views.upload_csv function
+    path('data/', views.display_data, name='display_data'),
+    path('get_data/', views.get_data, name='get_data'),  # Add URL mapping for get_data view
 ]
+
--- BadgerHire2/data_analysis_app/experiment_data_processor/views.py
+++ BadgerHire2/data_analysis_app/experiment_data_processor/views.py
@@ -1,8 +1,59 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-import csv, io
-from .models import Experiment
+# from django.shortcuts import render, redirect
+# from django.http import HttpResponse
+# import csv, io
+# from .models import Experiment
+# from django.db.models import Avg
+
+# def upload_csv(request):
+#     if request.method == 'POST':
+#         csv_file = request.FILES['file']
+#         dataset = csv_file.read().decode('utf-8')
+#         io_string = io.StringIO(dataset)
+#         next(io_string)  # Skip the header
+#         for column in csv.reader(io_string, delimiter=',', quotechar='"'):
+#             _, created = Experiment.objects.update_or_create(
+#                 name=column[0],
+#                 date=column[1],
+#                 observation_type=column[2],
+#                 value=float(column[3])
+#             )
+#         return redirect('display_data')  # Redirect to the display_data URL
+#     return render(request, 'experiment_data_processor/upload_csv.html')
+
+# def display_data(request):
+#     data = Experiment.objects.values('name', 'date', 'observation_type').annotate(avg_value=Avg('value')).order_by('name', 'date', 'observation_type')
+#     return render(request, 'experiment_data_processor/display_data.html', {'data': data})
+
+# from django.shortcuts import render, redirect
+# from django.http import HttpResponse
+# import csv, io
+# from .models import Experiment
+# from django.db.models import Avg
+
+# def upload_csv(request):
+#     if request.method == 'POST':
+#         csv_file = request.FILES['file']
+#         dataset = csv_file.read().decode('utf-8')
+#         io_string = io.StringIO(dataset)
+#         next(io_string)  # Skip the header
+#         for column in csv.reader(io_string, delimiter=',', quotechar='"'):
+#             _, created = Experiment.objects.update_or_create(
+#                 name=column[0],
+#                 date=column[1],
+#                 observation_type=column[2],
+#                 value=float(column[3])
+#             )
+#         return redirect('display_data')  # Redirect to the display_data URL
+#     return render(request, 'experiment_data_processor/upload_csv.html')
+
+# def display_data(request):
+#     data = Experiment.objects.values('name', 'date', 'observation_type').annotate(avg_value=Avg('value')).order_by('name', 'date', 'observation_type')
+#     return render(request, 'experiment_data_processor/display_data.html', {'data': data})
+
+from django.shortcuts import render
+from django.http import JsonResponse
 from django.db.models import Avg
+from .models import Experiment
 
 def upload_csv(request):
     if request.method == 'POST':
@@ -21,6 +72,9 @@ def upload_csv(request):
     return render(request, 'experiment_data_processor/upload_csv.html')
 
 def display_data(request):
-    data = Experiment.objects.values('name', 'date', 'observation_type').annotate(avg_value=Avg('value')).order_by('name', 'date', 'observation_type')
-    return render(request, 'experiment_data_processor/display_data.html', {'data': data})
+    return render(request, 'experiment_data_processor/display_data.html')
+
+def get_data(request):
+    data = list(Experiment.objects.values('name', 'date', 'observation_type').annotate(avg_value=Avg('value')).order_by('name', 'date', 'observation_type'))
+    return JsonResponse(data, safe=False)
 
