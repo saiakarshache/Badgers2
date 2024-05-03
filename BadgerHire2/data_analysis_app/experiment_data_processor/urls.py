5c25164743b9d860897b874de6a792dbfbd37ffe
saiakarshache <168671202+saiakarshache@users.noreply.github.com>
2024-05-02 22:41:18 -0500

    Final updates done
    everything is working and ui is fixed

--- BadgerHire2/data_analysis_app/experiment_data_processor/urls.py
+++ BadgerHire2/data_analysis_app/experiment_data_processor/urls.py
@@ -1,38 +1,3 @@
-# from django.urls import path
-# from .views import upload_csv, display_data
-
-# urlpatterns = [
-#     path('upload/', upload_csv, name='upload_csv'),
-#     path('data/', display_data, name='display_data'),
-# ]
-
-# from django.urls import path
-# from . import views  # Import the views module from the current package
-
-# urlpatterns = [
-#     path('upload/', views.upload_csv, name='upload_csv'),  # Use the views.upload_csv function
-#     path('data/', views.display_data, name='display_data'),
-# ]
-
-# from django.urls import path
-# from . import views  # Import the views module from the current package
-
-# urlpatterns = [
-#     path('upload/', views.upload_csv, name='upload_csv'),  # Use the views.upload_csv function
-#     path('data/', views.display_data, name='display_data'),
-#     path('get_data/', views.get_data, name='get_data'),  # Add URL mapping for get_data view
-# ]
-
-
-# from django.urls import path
-# from . import views
-
-# urlpatterns = [
-#     path('upload/', views.upload_csv, name='upload_csv'),
-#     path('data/', views.display_data, name='display_data'),
-#     path('get_data/', views.get_data, name='get_data'), 
-# ]
-
 from django.urls import path
 from . import views
 
